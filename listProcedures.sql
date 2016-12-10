53f38f068eb09f3da0e40d0885257f1a2b4cb105
David\DavidPC <a290206@uabc.edu.mx>
2016-12-09 17:14:53 -0800

    list added

---
+++ listProcedures.sql
@@ -0,0 +1,17 @@
+-- list of procedures of the Article Table
+
+-- call ArticleReport(); --report innner join with inventary 
+
+-- call AddArticle(id,price,'name','Characteristics','Trademark',year); --add a new article
+-- call deleteArticle(id);
+-- call ArticleList('Name of alrticle');
+-- call EditArticle(id,new price,'new name',' new Characteristics','new Trademark',year)
+
+
+
+-- list of procedures of the Users Table
+
+-- call AddUser(id,'name','LastName','Password'); --add a new article
+-- call deleteUser(id);
+-- call UserList('Name of alrticle');
+-- call EditUser(id,new price,'new name',' new Characteristics','new Trademark',year)
